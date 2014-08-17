281a6461f7ff8dc5fb2c046e7c8b2529db46ec38
huminaboz <huminaboz@gmail.com>
2014-08-16 23:51:44 -0500

    implemented tile art from collin so far. Created a test level with obstacles and tiles and clouds and bones. Now ready for a level design to fool around with.

--- Assets/Scripts/DestroyMeOnTouch.cs
+++ Assets/Scripts/DestroyMeOnTouch.cs
@@ -4,6 +4,10 @@ using System.Collections;
 public class DestroyMeOnTouch : MonoBehaviour {
 
 	void OnTriggerEnter2D ( Collider2D col ){
-		Destroy (gameObject);
+
+		if (col.gameObject.tag == "Player") {
+
+						Destroy (gameObject);
+				}
 	}
 }
