943de86eabb8f31b5d77986e2f71e034e3b98db7
huminaboz <huminaboz@gmail.com>
2014-08-17 13:09:32 -0500

    chests animate to open and a bone comes out

---
+++ Assets/Scripts/Popup.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Popup : MonoBehaviour {
+
+	//private float upwardsness = 1;
+
+	// Use this for initialization
+	void Start () {
+		//transform.position = transform.position + new Vector3 (0, 1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.position = transform.position + new Vector3 (0, Mathf.Lerp (0, 1, Time.deltaTime), 0);
+
+		Destroy(gameObject,1f);
+	}
+}
--- Assets/Scripts/Powerup1Script.cs
+++ Assets/Scripts/Powerup1Script.cs
@@ -3,12 +3,28 @@ using System.Collections;
 
 public class Powerup1Script : MonoBehaviour {
 
+	//Animation Stuff
+	private tk2dSpriteAnimator anim;
+
+	//Goodies
+	public tk2dSprite goodie;
+
+	void Start()
+	{
+		//Animation
+		anim = GetComponentInChildren<tk2dSpriteAnimator>();
+	}
+
 	void OnTriggerEnter2D(Collider2D other)
 	{
 		if (other.gameObject.tag == "Player") 
 		{
+
+
 			AudioHelper.CreatePlayAudioObject (BaseSoundManager.baseSoundManagerInstance.getItem, 1f, "getItemSoundObject" );
-			Destroy(gameObject);
+			anim.Play("chestOpen");
+			Instantiate(goodie, gameObject.transform.position, Quaternion.identity);
+			Destroy(gameObject, .25f);
 		}
 	}
 }
