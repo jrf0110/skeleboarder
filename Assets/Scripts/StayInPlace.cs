0f19bcefcd9113ba62e7f2e2ae3947aa82286f41
huminaboz <huminaboz@gmail.com>
2014-08-19 00:13:21 -0500

    Monsters with slime and Spiked Ball spins and trips you

---
+++ Assets/Scripts/StayInPlace.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StayInPlace : MonoBehaviour {
+
+	Vector2 spawnPosition;		
+
+	// Use this for initialization
+	void Start () {
+		spawnPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = spawnPosition;
+	}
+}
